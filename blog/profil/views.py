921a933d0e84232cd3152c381d7f98b5b87856f8
antoniusrikihermawan <rikihermawan9342@gmail.com>
2025-04-21 17:37:06 +0700

    Menambahkan halaman abut

--- blog/profil/views.py
+++ blog/profil/views.py
@@ -7,4 +7,11 @@ def home(request):
     return render(request,'index.html')
 
 def about(request):
-    return render(request, 'profil/index.html')
+    judul = 'about'
+    isi = 'Ini bagian isinya'
+    data = {
+        'title' : judul,
+        'isi' : isi,
+    }
+
+    return render(request,'profil/index.html',data)
